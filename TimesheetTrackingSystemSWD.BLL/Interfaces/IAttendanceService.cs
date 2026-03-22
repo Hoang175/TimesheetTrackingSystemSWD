42f6504330f5e678eaabf67fdb08f9edaf41db6a
hieppm76 <hieppmhe180827@fpt.edu.vn>
2026-03-22 12:53:07 +0700

    Daily Attendance

---
+++ TimesheetTrackingSystemSWD.BLL/Interfaces/IAttendanceService.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Threading.Tasks;
+using TimesheetTrackingSystemSWD.DAL.Models;
+
+namespace TimesheetTrackingSystemSWD.BLL.Interfaces
+{
+    public interface IAttendanceService
+    {
+        Task<AttendanceRecord?> GetTodayAttendanceAsync(int userId);
+        Task<bool> CheckInAsync(int userId, string ipAddress);
+        Task<bool> CheckOutAsync(int userId, string ipAddress);
+        Task<IEnumerable<AttendanceRecord>> GetMonthlyAttendanceAsync(int userId, int year, int month);
+    }
+}
---
+++ TimesheetTrackingSystemSWD.DAL/Repositories/AttendanceRepository.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TimesheetTrackingSystemSWD.DAL.Interfaces;
+using TimesheetTrackingSystemSWD.DAL.Models;
+
+namespace TimesheetTrackingSystemSWD.DAL.Repositories
+{
+    public class AttendanceRepository : IAttendanceRepository
+    {
+        private readonly TimesheetTrackingSystemSwdContext _context;
+
+        public AttendanceRepository(TimesheetTrackingSystemSwdContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<AttendanceRecord>> GetAttendanceRecordsByEmployeeIdAndPeriodAsync(int employeeId, DateOnly startDate, DateOnly endDate)
+        {
+            return await _context.AttendanceRecords
+                .Where(a => a.EmployeeId == employeeId && a.WorkDate >= startDate && a.WorkDate <= endDate && a.IsDeleted == false)
+                .OrderBy(a => a.WorkDate)
+                .ToListAsync();
+        }
+
+        public async Task<AttendanceRecord?> GetAttendanceRecordByIdAsync(int attendanceId)
+        {
+            return await _context.AttendanceRecords.FirstOrDefaultAsync(a => a.AttendanceId == attendanceId && a.IsDeleted == false);
+        }
+
+        public async Task UpdateAttendanceRecordAsync(AttendanceRecord record)
+        {
+            _context.AttendanceRecords.Update(record);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task AddAttendanceRecordAsync(AttendanceRecord record)
+        {
+            _context.AttendanceRecords.Add(record);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
